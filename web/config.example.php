343ac6ada7e6703fac2eea52858a1d611e05fd9a
Dinh Cong Tuan <dinhcongtuan2005@gmail.com>
2026-07-19 11:42:17 +0700

    change name page config

--- web/config.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Copy this file to config.php and replace the placeholder values.
-return [
-    'host' => 'localhost',
-    'database' => 'esp32_supervision',
-    'user' => 'database_user',
-    'password' => 'database_password',
-];
